95ee22cb4725d9229e4bb3008820e2b0e0f04b17
nadja <nadja.flam@gmx.net>
2021-04-07 15:50:42 +0800

    debugging

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,18 +1,18 @@
-var city="";
+var city = "";
 // variable declaration
 var searchCity = $("#search-city");
 var searchButton = $("#search-button");
 var clearButton = $("#clear-history");
 var currentCity = $("#current-city");
 var currentTemperature = $("#temperature");
-var currentHumidty= $("#humidity");
-var currentWSpeed=$("#wind-speed");
-var currentUvindex= $("#uv-index");
-var sCity=[];
+var currentHumidty = $("#humidity");
+var currentWSpeed = $("#wind-speed");
+var currentUvindex = $("#uv-index");
+var sCity = [];
 // searches the city to see if it exists in the entries from the storage
 function find(c){
-    for (var i=0; i<sCity.length; i++){
-        if(c.toUpperCase()===sCity[i]){
+    for (var i = 0; i<sCity.length; i++){
+        if(c.toUpperCase() === sCity[i]){
             return -1;
         }
     }
@@ -24,8 +24,8 @@ const APIKey="956dda6b020b9fbad71791c3c8fdfe53";
 // Display the curent and future weather to the user after grabing the city form the input text box.
 function displayWeather(event){
     event.preventDefault();
-    if(searchCity.val().trim()!==""){
-        city=searchCity.val().trim();
+    if(searchCity.val().trim() !== ""){
+        city = searchCity.val().trim();
         currentWeather(city);
     }
 }
@@ -33,7 +33,7 @@ function displayWeather(event){
 // Here we create the AJAX call
 function currentWeather(city){
     // Here we build the URL so we can get a data from server side.
-    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
     $.ajax({
         url:queryURL,
         method:"GET",
@@ -42,10 +42,10 @@ function currentWeather(city){
         // parse the response to display the current weather including the City name. the Date and the weather icon. 
         console.log(response);
         //Dta object from server side Api for icon property.
-        var weathericon= response.weather[0].icon;
-        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        var weathericon = response.weather[0].icon;
+        var iconurl = "https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
         // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-        var date=new Date(response.dt*1000).toLocaleDateString();
+        var date = new Date(response.dt*1000).toLocaleDateString();
         //parse the response for name of city and concanatig the date and icon.
         $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
         // parse the response to display the current temperature.
@@ -56,18 +56,18 @@ function currentWeather(city){
         // Display the Humidity
         $(currentHumidty).html(response.main.humidity+"%");
         //Display Wind speed and convert to MPH
-        var ws=response.wind.speed;
-        var windsmph=(ws*2.237).toFixed(1);
+        var ws = response.wind.speed;
+        var windsmph = (ws*2.237).toFixed(1);
         $(currentWSpeed).html(windsmph+"MPH");
         // Display UVIndex.
         //By Geographic coordinates method and using appid and coordinates as a parameter we are going build our uv query url inside the function below.
         UVIndex(response.coord.lon,response.coord.lat);
         forecast(response.id);
-        if(response.cod==200){
-            sCity=JSON.parse(localStorage.getItem("cityname"));
+        if(response.cod == 200){
+            sCity = JSON.parse(localStorage.getItem("cityname"));
             console.log(sCity);
-            if (sCity==null){
-                sCity=[];
+            if (sCity == null){
+                sCity = [];
                 sCity.push(city.toUpperCase()
                 );
                 localStorage.setItem("cityname",JSON.stringify(sCity));
@@ -87,7 +87,7 @@ function currentWeather(city){
     // This function returns the UVIindex response.
 function UVIndex(ln,lt){
     //lets build the url for uvindex.
-    var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+    var uvqURL = "https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
     $.ajax({
             url:uvqURL,
             method:"GET"
@@ -98,20 +98,20 @@ function UVIndex(ln,lt){
     
 // Here we display the 5 days forecast for the current city.
 function forecast(cityid){
-    var dayover= false;
-    var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+    var dayover = false;
+    var queryforcastURL = "https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
     $.ajax({
         url:queryforcastURL,
         method:"GET"
     }).then(function(response){
         
         for (i=0;i<5;i++){
-            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
-            var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
-            var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
-            var tempK= response.list[((i+1)*8)-1].main.temp;
-            var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
-            var humidity= response.list[((i+1)*8)-1].main.humidity;
+            var date = new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var iconcode = response.list[((i+1)*8)-1].weather[0].icon;
+            var iconurl = "https://openweathermap.org/img/wn/"+iconcode+".png";
+            var tempK = response.list[((i+1)*8)-1].main.temp;
+            var tempF = (((tempK-273.5)*1.80)+32).toFixed(2);
+            var humidity = response.list[((i+1)*8)-1].main.humidity;
         
             $("#fDate"+i).html(date);
             $("#fImg"+i).html("<img src="+iconurl+">");
@@ -131,9 +131,9 @@ function addToList(c){
 
 // display the past search again when the list group item is clicked in search history
 function invokePastSearch(event){
-    var liEl=event.target;
+    var liEl = event.target;
     if (event.target.matches("li")){
-        city=liEl.textContent.trim();
+        city = liEl.textContent.trim();
         currentWeather(city);
     }
 }
@@ -142,12 +142,12 @@ function invokePastSearch(event){
 function loadlastCity(){
     $("ul").empty();
     var sCity = JSON.parse(localStorage.getItem("cityname"));
-    if(sCity!==null){
-        sCity=JSON.parse(localStorage.getItem("cityname"));
-        for(i=0; i<sCity.length;i++){
+    if(sCity !== null){
+        sCity = JSON.parse(localStorage.getItem("cityname"));
+        for(i = 0; i<sCity.length;i++){
             addToList(sCity[i]);
         }
-        city=sCity[i-1];
+        city = sCity[i-1];
         currentWeather(city);
     }
 
@@ -156,7 +156,7 @@ function loadlastCity(){
 //Clear the search history from the page
 function clearHistory(event){
     event.preventDefault();
-    sCity=[];
+    sCity = [];
     localStorage.removeItem("cityname");
     document.location.reload();
 
